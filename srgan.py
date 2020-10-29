1fcd0f233c83375b4d07b79d59da5ea782a75f4b
shaynedlima <dlimashayne@gmail.com>
2020-10-29 12:19:16 +1100

    Cleaned up code

--- srgan.py
+++ srgan.py
@@ -19,28 +19,8 @@ def forward_pass(lr_img, bucket_name, models, filename, halve=False):
     srgan_generator.train(False)
     srgan_generator.eval()
     
-
-    # # Model checkpoints
-    # srgan_checkpoint = os.path.join(models, "checkpoint_srgan_2.pth.tar")
-    # # Load models
-    # srgan_generator = torch.load(srgan_checkpoint)['generator'].to(device)
-    # srgan_generator.eval()
-
-    # img = "./img/New Images/airport_lr.jpg"
-
-    # Load image, downsample to obtain low-res version
-    # hr_img = Image.open(img, mode="r")
-    # hr_img = hr_img.convert('RGB')
-
-     # if halve:
-    #     hr_img = hr_img.resize((int(hr_img.width / 2), int(hr_img.height / 2)),
-    #                            Image.LANCZOS)
-    # lr_img = hr_img.resize((int(hr_img.width / 4), int(hr_img.height / 4)),
-    #                        Image.BILINEAR)
-    
     # Upload LR Image
     upload_gcp(bucket_name, lr_img, f"lr_{filename}")
-    # lr_img.save(os.path.join(results, f"lr_{filename}"))
 
     # Super-resolution (SR) with SRGAN
     sr_img_srgan = srgan_generator(convert_image(lr_img, source='pil', target='imagenet-norm').unsqueeze(0).to(device))
